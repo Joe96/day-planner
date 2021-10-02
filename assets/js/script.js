120d58c0afb70450d51d6e98819bc690d0643c63
Joel Afamefune <jafamefune@gmail.com>
2021-10-01 19:08:35 -0500

    Display schedule.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,146 +1,146 @@
-var schedule = [
+var _24hours = [
     {
         id: "0",
-        hour: "09",
-        time: "09",
-        note: ""
-    },
-    {
-        id: "1",
-        hour: "10",
-        time: "10",
-        note: ""
-    },
-    {
-        id: "2",
-        hour: "11",
-        time: "11",
-        note: ""
-    },
-    {
-        id: "3",
-        hour: "12",
+        hour: "0:00",
         time: "12",
         note: ""
     },
     {
-        id: "4",
-        hour: "01",
-        time: "13",
-        note: ""
-    },
-    {
-        id: "5",
-        hour: "02",
-        time: "14",
-        note: ""
-    },
-    {
-        id: "6",
-        hour: "03",
-        time: "15",
-        note: ""
-    },
-    {
-        id: "7",
-        hour: "04",
-        time: "16",
-        note: ""
-    },
-    {
-        id: "8",
-        hour: "05",
-        time: "17",
-        note: ""
-    },
-    {
-        id: "9",
-        hour: "06",
-        time: "18",
-        note: ""
-    },
-    {
-        id: "10",
-        hour: "07",
-        time: "19",
-        note: ""
-    },
-    {
-        id: "11",
-        hour: "08",
-        time: "20",
-        note: ""
-    },
-    {
-        id: "12",
-        hour: "09",
-        time: "21",
-        note: ""
-    },
-    {
-        id: "13",
-        hour: "10",
-        time: "22",
-        note: ""
-    },
-    {
-        id: "14",
-        hour: "11",
-        time: "23",
-        note: ""
-    },
-    {
-        id: "15",
-        hour: "12",
-        time: "00",
-        note: ""
-    },
-    {
-        id: "16",
-        hour: "01",
+        id: "1",
+        hour: "1:00",
         time: "01",
         note: ""
     },
     {
-        id: "17",
-        hour: "02",
+        id: "2",
+        hour: "2:00",
         time: "02",
         note: ""
     },
     {
-        id: "18",
-        hour: "03",
+        id: "3",
+        hour: "3:00",
         time: "03",
         note: ""
     },
     {
-        id: "19",
-        hour: "04",
+        id: "4",
+        hour: "4:00",
         time: "04",
         note: ""
     },
     {
-        id: "20",
-        hour: "05",
+        id: "5",
+        hour: "5:00",
         time: "05",
         note: ""
     },
     {
-        id: "21",
-        hour: "06",
+        id: "6",
+        hour: "6:00",
         time: "06",
         note: ""
     },
     {
-        id: "22",
-        hour: "07",
+        id: "7",
+        hour: "7:00",
         time: "07",
         note: ""
     },
     {
-        id: "23",
-        hour: "08",
+        id: "8",
+        hour: "8:00",
         time: "08",
+        note: ""
+    },
+    {
+        id: "9",
+        hour: "9:00",
+        time: "09",
+        note: ""
+    },
+    {
+        id: "10",
+        hour: "10:00",
+        time: "10",
+        note: ""
+    },
+    {
+        id: "11",
+        hour: "11:00",
+        time: "11",
+        note: ""
+    },
+    {
+        id: "12",
+        hour: "12:00",
+        time: "12",
+        note: ""
+    },
+    {
+        id: "13",
+        hour: "13:00",
+        time: "13",
+        note: ""
+    },
+    {
+        id: "14",
+        hour: "14:00",
+        time: "14",
+        note: ""
+    },
+    {
+        id: "15",
+        hour: "15:00",
+        time: "15",
+        note: ""
+    },
+    {
+        id: "16",
+        hour: "16:00",
+        time: "16",
+        note: ""
+    },
+    {
+        id: "17",
+        hour: "17:00",
+        time: "17",
+        note: ""
+    },
+    {
+        id: "18",
+        hour: "18:00",
+        time: "18",
+        note: ""
+    },
+    {
+        id: "19",
+        hour: "19:00",
+        time: "19",
+        note: ""
+    },
+    {
+        id: "20",
+        hour: "20:00",
+        time: "20",
+        note: ""
+    },
+    {
+        id: "21",
+        hour: "21:00",
+        time: "21",
+        note: ""
+    },
+    {
+        id: "22",
+        hour: "22:00",
+        time: "22",
+        note: ""
+    },
+    {
+        id: "23",
+        hour: "23:00",
+        time: "23",
         note: ""
     },
     
@@ -156,7 +156,7 @@ function init() {
     var scheduleToday = JSON.parse(localStorage.getItem("today"));
 
     if (scheduleToday) {
-        schedule = scheduleToday;
+        _24hours = scheduleToday;
     }
 
     todo();
@@ -164,11 +164,57 @@ function init() {
 }
 
 function todo() {
-    localStorage.setItem("today", JSON.stringify(schedule));
+    localStorage.setItem("today", JSON.stringify(_24hours));
 }
 
 function displaySchedule() {
-    schedule.forEach(function (currentHour) {
+    _24hours.forEach(function (currentHour) {
         $(`#${currentHour.id}`).val(currentHour.note);
     })
 }
+
+_24hours.forEach(function(currentTime) {
+
+    var timeBlock = $("<form>").attr({
+        "class": "row"
+        
+    });
+
+    $(".container").append(timeBlock);
+
+    var hour = $("<div>")
+        .text(`${currentTime.hour}`)
+        .attr({
+            "class": "col-md-2 hour"
+    });
+
+    var schedule = $("<div>")
+        .attr({
+            "class": "col-md-9 description p-0"
+    });
+
+    var scheduleText = $("<textarea>");
+    schedule.append(scheduleText);
+    scheduleText.attr("id", currentTime.id);
+    if (currentTime.time < moment().format("HH")) {
+        scheduleText.attr ({
+            "class": "past", 
+        })
+    } else if (currentTime.time === moment().format("HH")) {
+        scheduleText.attr({
+            "class": "present"
+        })
+    } else if (currentTime.time > moment().format("HH")) {
+        scheduleText.attr({
+            "class": "future"
+        })
+    }
+
+    var saveBtn = $("<i>Save</i>")
+    var saveSchedule = $("<button>")
+        .attr({
+            "class": "col-md-1 saveBtn"
+    });
+    saveSchedule.append(saveBtn);
+    timeBlock.append(hour, schedule, saveSchedule);
+})
